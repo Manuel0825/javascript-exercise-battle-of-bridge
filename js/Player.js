6aa206a312f091de948dc3e4640bec691675189d
Manuel Vallejo <manuelvallejo@Manuels-MBP.technicolor.net>
2023-10-24 21:32:38 +0200

    volviendome loco

--- js/Player.js
+++ js/Player.js
@@ -1,1 +1,20 @@
-class Player {}
+class Player {
+    constructor(name, life, attack){
+        this.name = name;
+        this.life = life;
+        this.attack = attack;
+    }
+    attackPlayer(player, diceValue){
+        Player.life -= this.diceValue * 6;
+    }
+    isDead (){
+        return this.life <=0;
+    }
+
+}
+
+
+
+ 
+
+
